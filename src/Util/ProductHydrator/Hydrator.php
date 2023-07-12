fa096319c87f0603296533e070c65d05acf1a932
s.proezdov <s.proezdov@gmail.com>
2023-07-12 17:50:30 +0200

    feature/proezdov add Product Dto Hydrator

---
+++ src/Util/ProductHydrator/Hydrator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Util\ProductHydrator;
+
+use App\Dto\Product;
+use App\Enum\FoodTypes;
+use App\Enum\MassMeasuresUnits;
+use RuntimeException;
+
+class Hydrator
+{
+    /**
+     * @throws RuntimeException //TODO use more specific exception class
+     */
+    public function hydrateProduct(array $item): Product
+    {
+        $id = $item['id'] ?? null;
+        $name = $item['name'] ?? null;
+        $quantity = $item['quantity'] ?? null;
+        $type = FoodTypes::tryFrom((string)($item['type'] ?? null));
+        $unit = MassMeasuresUnits::tryFrom((string)($item['unit'] ?? null));
+
+        if (!isset($id, $name, $quantity, $type, $unit)) {
+            throw new RuntimeException("Wrong product item definition in JSON request. id: $id; name: $name");
+        }
+
+        return new Product(
+            (int)$id,
+            (string)$name,
+            (int)$quantity,
+            $type,
+            $unit,
+        );
+    }
+}
