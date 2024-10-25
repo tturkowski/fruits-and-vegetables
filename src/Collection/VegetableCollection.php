9554a5c25753195b9088cdf50fdc34be8a8b0907
Tadg Keating-Webb <tadgkeating@gmail.com>
2024-10-25 15:05:38 +0700

    Added ORM package, created entities, migrations and collections

---
+++ src/Collection/VegetableCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Collection;
+
+class VegetableCollection implements CollectionInterface
+{
+    public function add(string $item, int $grams): void
+    {
+        // TODO: Implenent list
+    }
+
+    public function remove(string $item): void
+    {
+        // TODO: Implenent list
+    }
+
+    public function list(): array
+    {
+        // TODO: Implenent list
+        return [];
+    }
+}
